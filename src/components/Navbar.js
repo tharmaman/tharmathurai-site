7fc629564ab3fe7257c51bcf892908503e033594
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-10-13 02:54:06 -0400

    centered the ting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import {
-  Navbar,
-  NavbarBrand,
-} from 'reactstrap'
+import { Navbar, NavbarBrand } from 'reactstrap'
 
 export default class Example extends React.Component {
   render() {
     return (
-      <div>
-        <Navbar style={{backgroundColor:'WhiteSmoke'}} color="light" light expand="md">
-          <NavbarBrand href="/">logathasan tharmathurai</NavbarBrand>
-        </Navbar>
-      </div>
+      <Navbar
+        style={{ backgroundColor: 'WhiteSmoke'}}
+        color="light"
+        light
+        expand="md"
+        className="justify-content-center"
+      >
+        <NavbarBrand  href="/">logathasan tharmathurai</NavbarBrand>
+      </Navbar>
     )
   }
 }
